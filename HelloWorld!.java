31eb07d96fd17abe232259715485efd7d5dd8b7c
Akash Shinde <96474874+Mr-AkashShinde@users.noreply.github.com>
2022-02-17 11:15:01 +0530

    Create HelloWorld!.java

---
+++ HelloWorld!.java
@@ -0,0 +1,11 @@
+package com.code;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World");
+        }
+}        
